969d2034c40928729d38dc2b2ccbf1128ccd8247
Adriano Romero <adriano.romerofco@gmail.com>
2022-08-08 17:11:35 -0300

    implementação polimorfismo

--- src/Conta.java
+++ src/Conta.java
@@ -1,9 +1,19 @@
 
 public abstract class Conta implements IConta{
-	private int agencia;
-	private int numero;
-	private double saldo;
-
+	protected int agencia;
+	protected int numero;
+	protected double saldo;
+	protected Cliente cliente;
+	
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+	
+	public Conta(Cliente cliente) {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+	}
+	
 	public int getAgencia() {
 		return agencia;
 	}
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,5 +2,6 @@
 public class ContaCorrente extends Conta{
 
 	
+	
 }
 
