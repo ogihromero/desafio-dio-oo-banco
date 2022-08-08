54302559c51ad1e48ae5f798b256b5dbf22a31cb
Adriano Romero <adriano.romerofco@gmail.com>
2022-08-08 15:56:12 -0300

    implementação herança

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,33 +1,6 @@
 
-public class ContaCorrente {
-	private int agencia;
-	private int numero;
-	private double saldo;
-	
-	public void sacar( ) {
-		
-	}
-	
-	public void depositar( ) {
-		
-	}
-	
-	public void transferir( ) {
-			
-	}
+public class ContaCorrente extends Conta{
 
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-	
 	
 }
 
