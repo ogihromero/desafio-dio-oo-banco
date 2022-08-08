37606fe729714f30a8945f55f83739520393668d
Adriano Romero <adriano.romerofco@gmail.com>
2022-08-08 15:47:48 -0300

    aula encapsulamento
    geração getters and setters

--- src/Banco.java
+++ src/Banco.java
@@ -2,4 +2,14 @@
 public class Banco {
 	public String nome;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
 }
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,8 +1,8 @@
 
 public class ContaCorrente {
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
 	public void sacar( ) {
 		
@@ -15,5 +15,19 @@ public class ContaCorrente {
 	public void transferir( ) {
 			
 	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
 }
 
